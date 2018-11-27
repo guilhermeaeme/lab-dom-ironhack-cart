9ed2a81d4b13bb0e8ddf3fcc93d1b91b3cba0f6f
guilhermeaeme <guilhermeaeme@gmail.com>
2018-11-27 12:45:53 -0200

    getTotalPrice and deleteItem

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,6 @@
 function deleteItem(e){
-
+  var item = e.currentTarget.parentNode.parentNode;
+  item.remove();
 }
 
 function getPriceByProduct(itemNode){
@@ -11,7 +12,23 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var prices = document.getElementsByClassName('span-price');
+  var qtys = document.getElementsByClassName('qty');
+  var totals = document.getElementsByClassName('span-total-price');
 
+  var total = 0;
+
+  for(i=0; i<prices.length; i++) {
+    var price = Number(prices[i].innerHTML);
+    var qty = Number(qtys[i].value);
+    var subtotal = price * qty;
+
+    total += subtotal;
+
+    totals[i].innerHTML = subtotal.toFixed(2);
+  }
+
+  document.getElementById('total-price').innerHTML = total.toFixed(2);
 }
 
 function createQuantityInput(){
@@ -44,7 +61,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
